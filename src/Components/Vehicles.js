e6a0a340f8dc40c2ea8c41160f667a9e741d7a86
Jonathan <jon.kachappilly@gmail.com>
2023-02-10 23:41:42 -0500

    Started Vehicles Page

--- src/Components/Vehicles.js
+++ src/Components/Vehicles.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import NavBar from './NavBar';
+import './Vehicles.css';
+
+import starshipImage from '../Pictures/starshipImage.jpg'
 
 export default function Vehicles() {
   return (
     <>
-      <div>
-        <h1 style={{color:'black'}}>Vehicles</h1>
+      <div id='vehicles-bg' className='scrollable'>
+          <img id='starship' src={starshipImage}/>
+          <div className='center-contents'>
+            <h1 id='starship-title'>STARSHIP</h1>
+            <h4 id='starship-subtitle'>THE FUTURE OF SPACE TRAVEL</h4>
+          </div>
+          <div>
+            <p style={{color: 'white', font:'Times New Roman'}}>Starship DescriptionStarship</p>
+          </div>
+          <img id='arrow-down' src='https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png'/>
       </div>
     </>
   )
