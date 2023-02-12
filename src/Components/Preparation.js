b0b05d0a63c36e31e8e4c09e6746b7a2f119eb8a
joshuahevelow <joshhevelow@comcast.net>
2023-02-11 19:44:17 -0500

    Added preparation page

--- src/Components/Preparation.js
+++ src/Components/Preparation.js
@@ -1,12 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 import NavBar from './NavBar';
+import './Preparation.css';
 
 export default function Preparation() {
+  const [trainingSubheadingColor, setTrainingSubheadingColor] = useState("#FF614C");
+  const [safetySubheadingColor, setSafetySubheadingColor] = useState("white");
+  const [trainingAnimation, setTrainingAnimation] = useState("");
+  const [safetyAnimation, setSafetyAnimation] = useState("");
+
+
+  function switchInfo(heading){
+    if(heading == "training"){
+      setTrainingSubheadingColor("#FF614C");
+      setSafetySubheadingColor("white");
+      setTrainingAnimation("text-down");
+      setSafetyAnimation("text-down");
+    }else{
+      setTrainingSubheadingColor("white");
+      setSafetySubheadingColor("#FF614C");
+      setTrainingAnimation("text-up");
+      setSafetyAnimation("text-up");
+    }
+  }
+
   return (
     <>
-      <div>
-      <h1 style={{color:'black'}}>Sub1</h1>
+      <img className='bg-image' src="https://live.staticflickr.com/65535/51881823865_1bbf378950_b.jpg" />
+      <div className="page-top">
+        <h1 style={{fontSize: '40px'}}>Preparations <br/> <div className="heading-underline">&nbsp;</div></h1>
+        <h3 className='page-subheading' id="training-subheading" style={{color: trainingSubheadingColor}} onClick={() => switchInfo("training")}>Training</h3>
+        <h3 className='page-subheading' id='safety-subheading' style={{color: safetySubheadingColor}} onClick={() => switchInfo("safety")}>Safety</h3>
       </div>
+      <div className='page-info'>
+        <div style={{animation: `${trainingAnimation} 2s ease-out 1 forwards`}}>
+        <h1 id='info-heading-training'>Training</h1>
+        <p id='info-body'>All tourists who wish to ride with Horizon Ventures must attend our mandatory space 
+                                    safety seminar prior to scheduling a flight. This seminar will include procedures for the 
+                                    entrance and departure of a spacecraft, gravity training, intergalactic language, and more.
+                                    Upon completing the seminar, you will be required to take a final exam to ensure you are prepared
+                                    for space travel. Failing this test will result in a retaking of the space safety seminar. If
+                                    you have traveled with us in the past, you may skip the seminar and take the final exam right away.
+                                  </p>
+      </div>
+      <br/><br/><br/>
+      <div style={{animation: `${safetyAnimation} 2s ease-out 1 forwards`}}>
+        <h1 id='info-heading-safety'>Safety</h1>
+        <p id='info-body'>During space travel, all tourists are required to wear Horizon Ventures's custom space suit, custom
+                          designed to maximize both comfort and safety. Your suit will be supplied with a double tank of oxygen,
+                          ensuring that you will be given plenty of fresh air as you take your journey through the stars. Every
+                          flight is led by two pilots with countless years of experience, as well as 4 flight attendants to monitor
+                          and potential safety concerns. Our triple-cushioned seats, which are part of every one of our space vessels,
+                          reduce bumps and bruises by 99%, and our two-way seatbelts keep you secured to your seat, even in zero
+                          gravity!</p>
+      </div>
+    </div>
     </>
   )
 }
--- src/Components/Return.js
+++ src/Components/Return.js
@@ -10,8 +10,8 @@ export default function Return() {
       <div id='return-bg'>
           <img id='vehicle' src={"https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQzNi01LWtzYy05NnBjMTI5Ml8xLmpwZw.jpg?s=yJss6GdvYE3Smb7G9K7NHR4F0NAe8elYXx_47QkWDQ4"}/>
           <div className='return-box' style={{backgroundColor: 'rgba(50, 47, 64, 0.5)'}}>
-            <h1 id='return-title' style={{transform: 'translateY(-2vh)', textDecoration: 'underline', letterSpacing: '10px'}}>YOUR TRIP</h1>
-            <h4 id='return-subtitle' style={{transform: 'translateY(-10vh)'}}>LAUNCH & RECOVERY</h4>
+            <h1 id='return-title' style={{transform: 'translateY(17vh)', textDecoration: 'underline', letterSpacing: '10px'}}>YOUR TRIP</h1>
+            <h4 id='return-subtitle' style={{transform: 'translateY(-3vh)'}}>LAUNCH & RECOVERY</h4>
           </div>
           <div className='return-description' style={{marginBottom: '10vh'}}>
             <p className='return-text' style={{fontSize: '16px'}}>Arrive at the launch site at the posted time and confrim with Horizon Ventures employees that you are cleared for takeoff. Proceed to the staging room and board the rocket vehicle. Strap in, perform the pre-flight checks in accordance with your training, and get ready for the ride of your life. The launch should take around 10 minutes. Once in orbit, you can unbuckle and enjoy zero-g.</p>
@@ -24,7 +24,7 @@ export default function Return() {
           <div className='return-description'>
             <p className='return-speed' style={{marginLeft: '4vh', marginTop: '4vh'}}>THE RECOVERY</p>
             <img style={{width: 'fit-content', marginLeft: '2vh', float: 'left', transform: 'translateX(-10vw)'}} src={returnIcon} alt="rocket" />
-            <p className='return-text' style={{marginLeft: '4vh', marginTop: '3vh', transform: 'translateX(-10vw)', fontSize: '16px'}}>During the return process make sure to follow all instruction of your on-board Horizon Ventures captain & crew. Make sure you are securely fastened to your seat and brace for the g-forces of re-entry. Stay tight once we set down in the Pacific Ocean since we will send a boat to your location.</p>
+            <p className='return-text' style={{marginLeft: '4vh', marginTop: '3vh', transform: 'translateX(-10vw)', fontSize: '16px'}}>During the return process make sure to follow all instruction of your on-board Horizon Ventures captain & crew. Make sure you are securely fastened to your seat and brace for the g-forces of re-entry. Stay tight once we set down in the Pacific Ocean as we send a boat to your location.</p>
           </div>
           <div style={{display: 'flex', justifyContent: 'center'}}>
             <img id='return-arrow-down' src='https://enterprisefilmsllc.com/wp-content/uploads/2018/07/white-down-arrow-png-2.png'/>
